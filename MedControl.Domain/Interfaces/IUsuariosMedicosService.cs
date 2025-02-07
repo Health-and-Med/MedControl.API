673856941c6a4f825a32ce2a9914c4c206426682
Matheus <Matheus@DESKTOP-1M4EQOT>
2025-02-07 17:31:26 -0300

    criação delete

--- MedControl.Domain/Interfaces/IUsuariosMedicosService.cs
+++ MedControl.Domain/Interfaces/IUsuariosMedicosService.cs
@@ -5,9 +5,9 @@ namespace MedControl.Application.Services
     public interface IUsuariosMedicosService
     {
         Task CreateAsync(RequestCreateUsuarioMedicoModel medico, int medicoId);
+        Task DeleteAsync(int medicoId);
         Task<UsuariosMedicosModel> GetByIdAsync(int medicoId);
         Task<IEnumerable<UsuariosMedicosModel>> GetAllAsync();
         Task UpdateAsync(RequestUpdateUsuarioMedico user, int medicoId);
-        Task DeleteAsync(int medicoId);
     }
 }
